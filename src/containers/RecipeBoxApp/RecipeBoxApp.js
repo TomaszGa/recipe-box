594e4c2efd48611992c4d6b5d9609b37ad404803
TomaszGa <tomasz.galina@gmail.com>
2018-02-12 03:12:55 +0100

    remove boilerplate and start building main container

---
+++ src/containers/RecipeBoxApp/RecipeBoxApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class RecipeBoxApp extends Component {
+    state = {  }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default RecipeBoxApp;
